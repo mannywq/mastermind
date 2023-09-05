6c4aab08086f332bcbe89e17731a481744052bfa
Manny <manny.wetterqvist@outlook.com>
2023-09-05 21:27:02 +0900

    Refactored to fit Ruby style guide

--- logic.rb
+++ logic.rb
@@ -7,24 +7,19 @@ class Game
     @turns = 12
     @guess = nil
     @gameover = false
+    @match = 0
+    @partial = 0
   end
 
   def check_guess(guess)
-    match = 0
-    partial = 0
     @code.length.times do |i|
       if @code[i] == guess[i]
-        match += 1
-      elsif @code.include?(guess[i])
-        partial += 1
+        @match += 1
+      else
+        @code.include?(guess[i]) ? @partial += 1 : next
       end
     end
-    if match == @code.length
-      puts "Good job, the code #{@code} matches your guess #{guess}. Game finished with #{@turns} turns left"
-      @gameover = true
-    else
-      puts "You got #{match} correct guesses and #{partial} numbers correct but in the wrong position"
-    end
+    game_over?
   end
 
   def generate_code
@@ -37,22 +32,13 @@ class Game
   end
 
   def make_guess
+    guess = ''
     loop do
       guess = prompt "Guess the code (#{@turns} left): "
-      if guess.length != 4
-        puts 'Please input 4 digits'
-      elsif guess.match?(/\D/)
-        puts 'All characters must be numeric'
-      else
-        @turns -= 1
-        return guess
-      end
+      break if valid_code?(guess)
     end
-  end
-
-  def prompt(*args)
-    print(*args)
-    gets.chomp
+    @turns -= 1
+    guess
   end
 
   def play(player)
@@ -62,6 +48,34 @@ class Game
       check_guess(@guess)
 
       break if @gameover || @turns.zero?
+    end
+  end
+
+  private
+
+  def prompt(*args)
+    print(*args)
+    gets.chomp
+  end
+
+  def game_over?
+    if @match == @code.length
+      puts "Good job, the code #{@code} matches your guess #{@guess}. Game finished with #{@turns} turns left"
+      @gameover = true
+    else
+      puts "You got #{code} correct guesses and #{@partial} numbers correct but in the wrong position"
+    end
+  end
+
+  def valid_code?(code)
+    if code.length != 4
+      puts 'Please input 4 digits'
+      false
+    elsif code.match?(/\D/)
+      puts 'All characters must be numeric'
+      false
+    else
+      true
     end
   end
 end
